7aa5d99103945ac0a5a05fd5221e6d0387361a53
Evan Klemen <ejklemen@gmail.com>
2015-03-10 17:07:11 -0400

    added a services dir but trying things more simply

--- src/app/index.js
+++ src/app/index.js
@@ -1,12 +1,13 @@
 'use strict';
 
-angular.module('studyBuddy', ['restangular', 'ui.router'])
+angular.module('studyBuddy', ['restangular', 'ui.router', 'firebase'])
   .config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
       .state('home', {
         url: '/',
         templateUrl: 'app/main/main.html',
-        controller: 'MainCtrl'
+        controller: 'MainCtrl',
+        controllerAs: 'main'
       });
 
     $urlRouterProvider.otherwise('/');
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,6 +1,8 @@
 'use strict';
 
 angular.module('studyBuddy')
-  .controller('MainCtrl', function () {
-    
+  .controller('MainCtrl', function(){
+    var ref = new Firebase("https://study-buddy.firebaseio.com/");
+    var sync = $firebase(ref);
+    console.log(sync);
   });
