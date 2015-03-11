ae3f50ccbec5ffc963df85826d357e00e3862663
Evan Klemen <ejklemen@gmail.com>
2015-03-10 20:53:21 -0400

    changing back to older version of firebase

--- src/app/group/group.controller.js
+++ src/app/group/group.controller.js
@@ -2,8 +2,24 @@
 
 angular.module('studyBuddy')
   .controller('GroupCtrl', function(){
-    var ref = new Firebase("https://study-buddy.firebaseio.com/groups");
-    this.group = [{test: "data"}];
-    ref.push(this.group);
-    console.log(ref);
+    var ref = new Firebase("https://study-buddy.firebaseio.com/");
+    this.groupList = {};
+    this.addGroup = function(newSubject, newClass){
+        ref.push({subject: "a new Subject"});
+        this.newSubject = "";
+    };
+    
+    
   });
+
+//var ref = new Firebase('https://taskfire.firebaseio.com/tasklist');	
+//			var sync = $firebase(ref);	
+//			this.todoItems = {};
+//			this.addItem = function(newTodoItem){
+//				sync.$push({task: this.newTodoItem, completed: false});	
+//				this.newTodoItem = "";
+//					
+//			};	
+//			this.todoItems = sync.$asObject();	
+//		})
+
