c03a6fc4f4ef9c77b3a0d7a8bdaecd47f57b8625
Evan Klemen <ejklemen@gmail.com>
2015-03-11 13:46:02 -0400

    One item is being pushed into the new group object

--- src/app/group/group.controller.js
+++ src/app/group/group.controller.js
@@ -1,20 +1,21 @@
 'use strict';
 
 angular.module('studyBuddy')
-  .controller('GroupCtrl', function($firebase){
-    var ref = new Firebase("https://study-buddy.firebaseio.com/");
-    
-    
-  });
-
-//var ref = new Firebase('https://taskfire.firebaseio.com/tasklist');	
-//			var sync = $firebase(ref);	
-//			this.todoItems = {};
-//			this.addItem = function(newTodoItem){
-//				sync.$push({task: this.newTodoItem, completed: false});	
-//				this.newTodoItem = "";
-//					
-//			};	
-//			this.todoItems = sync.$asObject();	
-//		})
-
+    .controller('GroupCtrl', ["$firebaseArray", function($firebaseArray){
+        var ref = new Firebase('https://study-buddy.firebaseio.com/');
+        
+        this.groupList = [];
+        
+        this.newGroup = {
+            subject: '',
+            class: ''
+        };
+        
+        this.addGroup = function(newGroup){
+            ref.push(this.newGroup);
+            console.log(this.newGroup);
+        };
+        
+        
+    }])
+;
