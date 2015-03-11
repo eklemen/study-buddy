931bfd1cc9105773533b9f9c34c4ae398334f99e
Evan Klemen <ejklemen@gmail.com>
2015-03-11 13:00:36 -0400

    back to VERSION 1.0 of angularfire

--- src/app/group/group.controller.js
+++ src/app/group/group.controller.js
@@ -3,19 +3,7 @@
 angular.module('studyBuddy')
   .controller('GroupCtrl', function($firebase){
     var ref = new Firebase("https://study-buddy.firebaseio.com/");
-    var sync = $firebase(ref);
-    this.groupList = {};
-    this.newGroup = {
-        sub: '',
-        cl: ''
-    };
-    this.addGroup = function(newSubject, newClass){
-        sync.$push({sub: this.newSubject,
-                 cl: this.newClass});
-        this.newSubject = "";
-        this.newClass = "";
-    };
-    this.newGroup = sync.$asArray();
+    
     
   });
 
