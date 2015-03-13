020c91ecf97f0b6af2c546d8eeee534dcc07f0ec
Evan Klemen <ejklemen@gmail.com>
2015-03-12 22:43:32 -0400

    facebook now logs in, and goes to next route as well im so damn happy

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,4 +1,24 @@
 'use strict';
 
 angular.module('studyBuddy')
-    .controller('MainController', function(){});
+    .controller('MainController', function($firebaseArray, $firebaseObject, Auth){
+        var self = this;
+        var userInfo = new Firebase('https://study-buddy.firebaseio.com/users');
+        this.obj = $firebaseArray(userInfo);
+        console.log(this.obj);
+        this.userArray = {};
+        
+    this.fbLogin = Auth.fbLogin;
+    
+    Auth.onAuth(function(user){
+        self.user = user;
+        if(user === null){
+            console.log('null');
+        } else {
+            console.log(user);
+        }
+    });
+    
+    
+})
+; //end of MainController
