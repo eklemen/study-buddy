d9c01f33e02ca4f02c69fdd8cf84417cec293ee7
Evan Klemen <ejklemen@gmail.com>
2015-03-14 13:14:05 -0400

    form is being sent to firebase again, but not under the user, changed from firebaseObject to firebaseArray, this might be the key

--- src/app/group/group.controller.js
+++ src/app/group/group.controller.js
@@ -2,30 +2,29 @@
 
 angular.module('studyBuddy')
     .controller('GroupCtrl', function($firebaseObject, $firebaseArray, Auth){
-    this.logout = Auth.logout;
-    var self = this;
-    Auth.onAuth(function(user){
-        self.user = user;
-    });
-    console.log(self);
-    var ref = new Firebase('https://study-buddy.firebaseio.com/users/' + self.user.$id);
-    self.obj = $firebaseObject(ref); 
+//    this.logout = Auth.logout;
+////    var self = this;
+//    Auth.onAuth(function(user){
+//        self.user = user;
+//    });
+//    console.log(self.user);
+    var ref = new Firebase('https://study-buddy.firebaseio.com/users/');
+    this.obj = $firebaseArray(ref); 
         
-        self.newGroup = {
+    this.newGroup = {
             subject: '',
             class: '',
             section: ''
         };
-        console.log(self.obj);
+
         this.addGroup = function(group){
-            self.obj.update(group);
-            return self.newGroup = {
+            this.obj.$add(group);
+            return this.newGroup = {
                 subject: '',
                 class: '',
                 section: ''
             };
         };
-        
         
     })
 ;
