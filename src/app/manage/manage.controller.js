9e0e337d8a1b4c63da014de5ff07003a07787cfe
Evan Klemen <ejklemen@gmail.com>
2015-03-16 22:11:38 -0400

    All basic funtionality is done, now for styling

--- src/app/manage/manage.controller.js
+++ src/app/manage/manage.controller.js
@@ -1,14 +1,17 @@
 'use strict';
 
 angular.module('studyBuddy')
-    .controller('ManageController', function($firebaseArray, Auth){
+    .controller('ManageController', function($firebaseObject, Auth){
     var self = this;
     Auth.onAuth(function(user, authdUser){
         self.user = user;
     });
     
     var myRef = new Firebase('https://study-buddy.firebaseio.com/groups/' + self.user.$id);
-    this.arr = $firebaseArray(myRef);
+    this.arr = $firebaseObject(myRef);
     
+    this.edit = function(info){
+        myRef.update(info);
+    }
 })
 ; //end controller
