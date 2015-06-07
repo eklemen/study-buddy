352e19122a4d13c2620926f261c17832ef839303
Evan Klemen <ejklemen@gmail.com>
2015-06-07 17:19:26 -0400

    push

--- src/app/main/main.controller.spec.js
+++ src/app/main/main.controller.spec.js
@@ -10,35 +10,36 @@ describe('controllers', function(){
   }));
 
   it('should tell me things about MainController', inject(function($controller, Auth) {
-//    expect(scope.awesomeThings).toBeUndefined();
+    expect(scope.awesomeThings).toBeUndefined();
       
     $controller('MainController', {
+        $scope: scope
         
-      $scope: scope
 //        var self = this;
 //        expect(scope.obj.length > 1).toBeTruthy();
     });
       expect(1<3).toBeTruthy();
       expect(Auth.fbLogin).toBeTruthy();
+      
 //    expect(angular.isArray(scope.awesomeThings)).toBeTruthy();
 //    expect(scope.awesomeThings.length > 5).toBeTruthy();
   }));
 });
 
+// end describe 1
+
 describe('Auth', function () {
   // Load your module.
   beforeEach(module('studyBuddy'));
-
   // Setup the mock service in an anonymous module.
-//  beforeEach(module(function ($provide) {
-////    $provide.value('oneOfMyOtherServicesStub', {
-//    });
-//  }));
 
-  it('can get an instance of my factory', inject(function(Auth) {
+  it('Auth factory should be defined', inject(function(Auth) {
     expect(Auth).toBeDefined();
-    expect(Auth.fbLogin).toBeDefined(); 
-    dump(Auth.onAuth);
-    console.log(Auth.onAuth);
+//    expect(Auth.fbLogin).toBeDefined(); 
+//    dump(Auth.onAuth);
+//    console.log(Auth.onAuth);
   }));
+    it('Auth factory returns callbacks', inject(function(Auth) {
+        expect(Auth.fbLogin).toBeDefined(); 
+    }));
 });
